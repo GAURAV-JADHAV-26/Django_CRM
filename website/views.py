cce26a69b9ef067324db684229610e50b0c2c49d
Gaurav Jadhav <gauravj2604@gmail.com>
2025-03-09 16:32:51 +0530

    added record updating functionality and PROJECT IS DONE

--- website/views.py
+++ website/views.py
@@ -119,3 +119,26 @@ def add_record(request):
         messages.success(request, "You Must Be Logged In To Do That Action")
         return redirect('home')
 
+def update_record(request, pk):
+
+    if request.user.is_authenticated:
+
+        current_record = Record.objects.get(id = pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+
+        if request.method == "POST":
+
+            if form.is_valid():
+
+                form.save()
+                messages.success(request, "Record Updated Successfully")
+                return redirect('home')
+            
+
+        return render(request, 'update_record.html', {'form': form} )
+
+       
+       
+    else:
+        messages.success(request, "You Must Be Logged In To Do That Action")
+        return redirect('home')
