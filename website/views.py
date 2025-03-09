0f36bc33ff78ebde840856d90e58b4f1c7ca1f83
Gaurav Jadhav <gauravj2604@gmail.com>
2025-03-09 15:35:56 +0530

    created a delete record button and added deletion functionality

--- website/views.py
+++ website/views.py
@@ -80,4 +80,16 @@ def customer_record(request, pk):
         #Unauthorized access
         messages.success(request, "You Must Be Logged In To View That Page")
         return redirect('home')
-    pass
+
+
+def delete_record(request, pk):
+
+    if request.user.is_authenticated:
+
+        delete_it = Record.objects.get(id = pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To Do That Action")
+        return redirect('home')
